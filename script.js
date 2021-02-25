4249f1a5df081ba9b752c2afeccab8dabbfefd03
Julia Astakhova <walkaway1919@gmail.com>
2021-02-25 13:05:35 +0300

    add drag'n'drops

--- script.js
+++ script.js
@@ -175,6 +175,7 @@ function createCard( title, content, doneStatus, tags = {}){
 
    newCard.className = cardClasses.join(" ");
    newCard.innerHTML = `<div class="card-header">
+         <div class="drag-card">#</div>
          <div class="title-card">${title}</div>
          <div class="menu-card">
          <ul class="dropdown-menu">
@@ -204,6 +205,7 @@ function createCard( title, content, doneStatus, tags = {}){
          }
          localRefresh()
       });
+   document.querySelectorAll('.task-card').forEach(el => console.log(el))
    return newCard
 }
 
@@ -327,7 +329,6 @@ document.addEventListener("click", function(e){
 })
 
 
-
 document.addEventListener('DOMContentLoaded', function(){
 
    let cardList = JSON.parse( localStorage.getItem('todoList') )
@@ -341,5 +342,101 @@ document.addEventListener('DOMContentLoaded', function(){
 
    })
    changeLang()
-
 })
+const filler = document.createElement("div")
+filler.classList.add("card-filler")
+
+const dragObject = {
+   filler: filler,
+   element: null,
+   cloned: null,
+   dragger: null,
+   hover: [null, null],
+   calcPosition: function( element, [posX, posY] ){
+      if( !element ) return this.hover[0]
+      const leftRange = element.offsetLeft + element.offsetWidth / 2
+      const topRange = element.offsetTop + element.offsetHeight / 2
+
+     
+     if( window.width > 599 ){
+
+        if( posX > leftRange ){
+           return 'right'
+         }
+         return 'left'
+      } else {
+         
+        if( posY > topRange ){
+         return 'right'
+       }
+       return 'left'
+      }
+   },
+   drag: function(e) {
+      if( !this.cloned ){
+         this.cloned = this.element.cloneNode(true)
+         this.cloned.classList.add('cloned')
+         this.element.closest(".main").append( this.cloned )
+      }
+      this.element.style.display = 'none'
+      // this.element.after(this.filler)
+      // console.log('movement', this)
+
+      const hoveredCard = e.target.closest(".task-card")
+      const hoveredPos = this.calcPosition( hoveredCard, [e.pageX, e.pageX])
+
+      this.cloned.style.left = e.pageX - 40 + 'px'
+      this.cloned.style.top = e.pageY - 40 + 'px'
+
+      if( hoveredCard && (this.hover[0] !== hoveredPos || this.hover[1] !== hoveredCard) ){
+         this.hover = [
+            hoveredPos,
+            hoveredCard
+         ];
+         this.moveCard( this.filler )
+      }
+   },
+   moveCard( elem ){
+      if( this.hover[0] === 'left'){
+         this.hover[1].before(elem)
+      } else {
+         this.hover[1].after(elem)
+      }
+   },
+   unDrag(){
+      if( this.element ){
+         document.removeEventListener('mousemove', this.dragger)
+         this.moveCard( this.element )
+         this.element.style.display = 'block'
+         this.filler.remove()
+         this.cloned.remove()
+         this.element = null
+         this.cloned = null
+         this.hover = [
+            null,
+            null
+         ];
+         localRefresh()
+      }
+   }
+}
+document.addEventListener('mousedown', function(e){
+   const dragElem = e.target.closest(".drag-card")
+   if( dragElem ){
+      console.log('нажали')
+      e.preventDefault()
+      const element = dragElem.closest(".task-card")
+      dragObject.element = element
+      dragObject.dragger = dragObject.drag.bind(dragObject)
+      document.addEventListener('mousemove', dragObject.dragger)
+      // dragObject.downX = e.pageX;
+      // dragObject.downY = e.pageY;
+      // console.log(dragObject)
+   }
+})
+
+document.addEventListener('mouseup', function(e){
+   console.log('отжали')
+   dragObject.unDrag.apply(dragObject)
+})
+
